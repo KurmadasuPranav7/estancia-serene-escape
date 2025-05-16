557299cdb29587352139f1043fbd8015a14624f9
KurmadasuPranav7 <69029740+KurmadasuPranav7@users.noreply.github.com>
2025-05-16 15:27:05 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/estancia-serene-escape">
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
